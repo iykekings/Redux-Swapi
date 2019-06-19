6d286b22018d7b4a98058372157d712ff8da0452
iykekings <iykekings36@gmail.com>
2019-06-19 12:55:31 +0100

    complete starWarsReducer

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,6 +1,9 @@
 import /* we need our action types here*/ "../actions";
+import { FETCHING, SUCCESS, FAILURE } from "../actions";
 const initialState = {
-  characters: []
+  characters: [],
+  fetching: false,
+  error: null
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
@@ -8,6 +11,12 @@ export const charsReducer = (state = initialState, action) => {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case FETCHING:
+      return {characters: action.characters, fetching: true, error: state.error}
+    case SUCCESS:
+      return {...state, fetching: false}
+    case FAILURE:
+      return {...state, fetching: false, error: 'There was an error'}
     default:
       return state;
   }
